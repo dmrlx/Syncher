b5db49a0b5a31903e39161e0cdaa63c35f334112
dmrlx <demuraalex@gmail.com>
2017-03-22 00:14:08 +0300

    Changed tab for spaces in Parser.py

--- Parser.py
+++ Parser.py
@@ -1,41 +1,45 @@
 class Parser(object):
-	class Rsync_options():
-		@staticmethod
-		def do(some_list):
-			pattern = r'-\w+|--.+'
-			options = []
-			for element in some_list:
-				option = re.match(pattern, element)
-				if option:
-					options.append(option)
-					return ' '.join(options)
 
+    class Rsync_options():
 
-	class Directory():
-		@staticmethod
-		def do(some_list):
-			pattern = ''
+        @staticmethod
+        def do(some_list):
+            pattern = r'-\w+|--.+'
+            options = []
+            for element in some_list:
+                option = re.match(pattern, element)
+                if option:
+                    options.append(option)
+                    return ' '.join(options)
 
-	class Password():
-		@staticmethod
-		def do(some_list):
-			pattern = r'dont_know_about_password_thing'
-			for element in some_list:
-				password = re.match(pattern, element)
-				if password:
-					return password
+    class Directory():
 
-	class Files_without_star():
-		@staticmethod
-		def do(some_list):
-			pattern = r'.+\..+'
-			files = []
-			for element in some_list:
-				file = re.match(pattern, element)
-				if file:
-					files.append(file.group(0))
-			return ' '.join(files)
+        @staticmethod
+        def do(some_list):
+            pattern = ''
+
+    class Password():
+
+        @staticmethod
+        def do(some_list):
+            pattern = r'dont_know_about_password_thing'
+            for element in some_list:
+                password = re.match(pattern, element)
+                if password:
+                    return password
+
+    class Files_without_star():
+
+        @staticmethod
+        def do(some_list):
+            pattern = r'.+\..+'
+            files = []
+            for element in some_list:
+                file = re.match(pattern, element)
+                if file:
+                    files.append(file.group(0))
+            return ' '.join(files)
 
 import re
 
-print(Parser.Files_without_star.do(['45.123','qwer.ty','rte.']))
+print(Parser.Files_without_star.do(['45.123', 'qwer.ty', 'rte.']))
