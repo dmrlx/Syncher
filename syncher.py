15b312ba95ac36283b9afc905c8bd6c1ef824de1
dmrlx <demuraalex@gmail.com>
2017-03-21 12:37:16 +0300

    Added syncher.py and task.txt

---
+++ syncher.py
@@ -0,0 +1,25 @@
+class ArgsReceiver(object):
+    return result
+
+
+class ParserResults(object):
+    cli = ""
+    password = ""
+    files = []
+    host = ""
+
+
+class Parser(object):
+    class ParserPassword(object):
+        def parser(self, some_list):
+            return result
+
+    class ParserFiles(object):
+        def parser(self, some_list):
+            return result
+
+
+class ValidateParams(object):
+    def validator(self, ParsResult):
+        return result
+
