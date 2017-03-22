ad5366cbd17cbd33606ce7022fb08a84c3913e3c
vasilmaksimovich <basilioss@gmail.com>
2017-03-22 19:03:59 +0300

    Add files via upload

---
+++ ArgsReceiver.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+
+
+class ArgsReceiver(object):  # Получает аргументы от CLI
+
+    @staticmethod
+    def receiver():
+        return sys.argv[1:]
+
+
+print(ArgsReceiver.receiver())
