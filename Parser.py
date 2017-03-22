b2924238849ceb4c63f34426805f6683ed75d925
Evgeny <kirgenvall@gmail.com>
2017-03-22 21:28:58 +0300

    Не работает, работающая версия в history :)

--- Parser.py
+++ Parser.py
@@ -14,7 +14,7 @@ class ArgsReceiver:
         return some_list
 
 
-class ParserResults(object):
+class Parser_Results:
     cli = ""
     password = ""
     files = ""
@@ -25,7 +25,7 @@ class ParserResults(object):
     full_host = ""
 
 
-class Parser(object):
+class Parser:
 
     @staticmethod
     def check_for_match(pattern, some_list):
@@ -39,7 +39,7 @@ class Parser(object):
     class Rsync_options:
         @staticmethod
         def parser(some_list):
-            pattern = r'^(-\w+|--[\w\-\=]+/[\w\d\/\.]+)'
+            pattern = r'^(-\w+|--[\w\-\=]+[\w\d\/\.\_]+)'
             return Parser.check_for_match(pattern, some_list)
 
     class Local_directory:
@@ -51,7 +51,7 @@ class Parser(object):
     class Files:
         @staticmethod
         def parser(some_list):
-            pattern = r'^[^-].+\..+'
+            pattern = r'^[^-].+\.[^@].+'
             return Parser.check_for_match(pattern, some_list)
 
     @staticmethod
@@ -65,25 +65,30 @@ class Parser(object):
     class Remote_user:
         @staticmethod
         def parser(some_list):
+            pattern = r'^\w+[\:\.\,]'
             remote_stuff = Parser.remote_stuff(some_list)
-            user_plus_port = remote_stuff[0].split(':')
-            return user_plus_port[0]
+            if : in remote_stuff[0]:
+                print('yes')
+            user = re.match(pattern, remote_stuff[0])
+            print(user)
+            if user:
+                return user.group(0)
 
     class Remote_port:
         @staticmethod
         def parser(some_list):
+            pattern = r'\d+'
             remote_stuff = Parser.remote_stuff(some_list)
-            user_plus_port = remote_stuff[0].split(':')
-            if len(user_plus_port) > 1:
-                return user_plus_port[1]
-            else:
-                return ''
+            port = re.match(pattern, remote_stuff[0])
+            print(port)
+            if port:
+                return port.group(0)
 
     class Remote_host:
         @staticmethod
         def parser(some_list):
             remote_stuff = Parser.remote_stuff(some_list)
-            host_plus_dir = remote_stuff[1].split(':,.')
+            host_plus_dir = remote_stuff[1].split(':')
             return host_plus_dir[0]
 
     class Remote_directory:
@@ -96,21 +101,25 @@ class Parser(object):
             else:
                 return ''
 
-    ParserResults.cli = Rsync_options.parser(ArgsReceiver.receiver)
-    ParserResults.password = 'something'
-    ParserResults.files = Files.parser(ArgsReceiver.receiver)
-    ParserResults.name = Remote_user.parser(ArgsReceiver.receiver)
-    ParserResults.port = Remote_port.parcer(ArgsReceiver.receiver)
-    ParserResults.host = Remote_host.parser(ArgsReceiver.receiver)
-    ParserResults.dist = Remote_directory.parser(ArgsReceiver.receiver)
-    ParserResults.full_host = remote_stuff(ArgsReceiver.receiver)
+class Give:
+    @staticmethod
+    def parsing():
+        Parser_Results.cli = Parser.Rsync_options.parser(ArgsReceiver.receiver())
+        Parser_Results.password = 'something'
+        Parser_Results.files = Parser.Files.parser(ArgsReceiver.receiver())
+        Parser_Results.user = Parser.Remote_user.parser(ArgsReceiver.receiver())
+        Parser_Results.port = Parser.Remote_port.parser(ArgsReceiver.receiver())
+        Parser_Results.host = Parser.Remote_host.parser(ArgsReceiver.receiver())
+        Parser_Results.dist = Parser.Remote_directory.parser(ArgsReceiver.receiver())
+        Parser_Results.full_host = Parser.remote_stuff(ArgsReceiver.receiver())
 
 
+Give.parsing()
 print(sys.version)
-print(ParserResults.cli)
-print(ParserResults.files)
-print(ParserResults.user)
-print(ParserResults.port)
-print(ParserResults.host)
-print(ParserResults.dist)
-print(ParserResults.full_host)
+print(Parser_Results.cli)
+print(Parser_Results.files)
+print(Parser_Results.user)
+print(Parser_Results.port)
+print(Parser_Results.host)
+print(Parser_Results.dist)
+print(Parser_Results.full_host)
