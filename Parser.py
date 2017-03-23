496e3dd639b2b5976609e6a0354f656b5f8b2614
Evgeny <kirgenvall@gmail.com>
2017-03-23 19:45:46 +0300

    Update Parser.py

--- Parser.py
+++ Parser.py
@@ -1,20 +1,16 @@
-"""
 import re
 import sys
 
-
 class ArgsReceiver:
-
     @staticmethod
     def receiver():
         options_list = ['-a', '-av', '--pass-file=/take.here', "-pass='111'"]
-        dirs_list = ['/usr', '/usr/', 'host:/usr']
-        files_list = ['45.123', 'qwer.ty', 'rte.']
-        users_list = ['user:22@host:/usr']
+        dirs_list = ['/usr', '/usr/']
+        files_list = ['45.123', 'qwer.ty', 'e.t', 'word*']
+        users_list = ['user.13@host:/usr']
         some_list = options_list + dirs_list + files_list + users_list
         return some_list
-
-
+    
 class Parser_Results:
     cli = ""
     password = ""
@@ -24,9 +20,7 @@ class Parser_Results:
     port = ""
     host = ""
     dist = ""
-    full_host = ""
 
-"""
 class Parser(object):
 
     @staticmethod
@@ -34,7 +28,7 @@ class Parser(object):
         match_list = []
         for element in some_list:
             match = re.match(pattern, element)
-            if match:
+            if match or match is not None:
                 match_list.append(element)
         return " ".join(match_list)
 
@@ -45,8 +39,7 @@ class Parser(object):
             password_pattern = r'-pass=.+'
             options = Parser.check_for_match(options_pattern, some_list)
             password = Parser.check_for_match(password_pattern, some_list)
-            options = options.replace(password, '')
-            return options
+            return options.replace(password, '')
 
     class Password:
         @staticmethod
@@ -54,69 +47,71 @@ class Parser(object):
             pattern = r'-pass=.+'
             return Parser.check_for_match(pattern, some_list)
 
-
-# Final
     class Local_directory:
         @staticmethod
         def parser(some_list):
             pattern = r'^/.+'
             return Parser.check_for_match(pattern, some_list)
 
-# Final (Files have no digits in extentions)
+# Files have no digits in extentions
     class Files:
         @staticmethod
         def parser(some_list):
-            pattern = r'^[^-].+\.\D+'
-            return Parser.check_for_match(pattern, some_list)
+            pattern = r'^[^-\/]{1,2}.+\D+$'
+            found = Parser.check_for_match(pattern, some_list)
+            remote_stuff = Parser.remote_stuff(some_list)
+            return found.replace(remote_stuff, '')
 
+# Can be no user nor host at all (checked)
     @staticmethod
     def remote_stuff(some_list):
-        pattern = r'^.+@.+'
+        pattern = r'^.*@.*'
         for element in some_list:
             remote = re.match(pattern, element)
-            if remote:
+            if remote or remote is not None:
                 return element
+        return ''
 
-# Final?!
     class Remote_user:
         @staticmethod
         def parser(some_list):
-            pattern = r'^\w+[^\:\.\,]'
+            pattern = r'^\w+[^\:\.\,]*'
             remote_stuff = Parser.remote_stuff(some_list).split('@')
             user = re.match(pattern, remote_stuff[0])
-            if user:
+            if user or user is not None:
                 return user.group(0)
+            return ''
 
-# Final?!O_o
     class Remote_port:
         @staticmethod
         def parser(some_list):
             user = Parser.Remote_user.parser(some_list)
             remote_stuff = Parser.remote_stuff(some_list).split('@')
             port = remote_stuff[0].lstrip(user)
-            if port:
+            if port or port is not None:
                 port = port.lstrip(':,.')
             return port
 
-# Host may be ip-like
+# Host may be ip-like (i think)
     class Remote_host:
         @staticmethod
         def parser(some_list):
             remote_stuff = Parser.remote_stuff(some_list).split('@')
-            host_plus_dir = remote_stuff[1].split(':')
-            return host_plus_dir[0]
+            if len(remote_stuff) > 1:
+                host_plus_dir = remote_stuff[1].split(':')
+                return host_plus_dir[0]
+            return ''
 
     class Remote_directory:
         @staticmethod
         def parser(some_list):
             remote_stuff = Parser.remote_stuff(some_list).split('@')
-            host_plus_dir = remote_stuff[1].split(':')
-            if len(host_plus_dir) > 1:
-                return host_plus_dir[1]
-            else:
-                return ''
+            if len(remote_stuff) > 1:
+                host_plus_dir = remote_stuff[1].split(':')
+                if len(host_plus_dir) > 1:
+                    return host_plus_dir[1]
+            return ''
 
-"""
 class Throw_in:
     @staticmethod
     def parser_results():
@@ -128,9 +123,7 @@ class Throw_in:
         Parser_Results.port = Parser.Remote_port.parser(ArgsReceiver.receiver())
         Parser_Results.host = Parser.Remote_host.parser(ArgsReceiver.receiver())
         Parser_Results.dist = Parser.Remote_directory.parser(ArgsReceiver.receiver())
-        Parser_Results.full_host = Parser.remote_stuff(ArgsReceiver.receiver())
-
-
+        
 Throw_in.parser_results()
 print(sys.version)
 print(Parser_Results.cli)
@@ -141,5 +134,3 @@ print(Parser_Results.user)
 print(Parser_Results.port)
 print(Parser_Results.host)
 print(Parser_Results.dist)
-print(Parser_Results.full_host)
-"""
