052181f64a4a93225ae9a3f8e8dc4a70c410a5f9
dmrlx <demuraalex@gmail.com>
2017-03-23 22:57:55 +0300

    Updates in validator class

--- Validator.py
+++ Validator.py
@@ -1,29 +1,29 @@
-class ValidateParams(object):       
+class ValidateParams(object):
 
     @staticmethod
-    def Check_lenth(parametr):
-        magic_number=777
+    def Check_length(parametr, param_name):
+        magic_number = 777
         try:
-            magic_number/len(parametr)
+            magic_number / len(parametr)
             return parametr
         except:
-            print('Unfortunately, you forgot define {}!\nFormat rsync function : rsync [OPTION] ... SRC ... [USER@] HOST:DEST \nPlease, try again'.format(parametr))
+            print('Unfortunately, you forgot define {}!\nFormat rsync function : rsync [OPTION] ... SRC ... [USER@] HOST:DEST \nPlease, try again'.format(param_name))
             sys.exit(1)
-            
+
     class Source_files:
         @staticmethod
         def validator():
-            return ValidateParams.Check_lenth(Parser_Results.loc+Parser_Results.files)
+            return ValidateParams.Check_length(ParserResults.loc + ParserResults.files, "files")
 
     class Username:
         @staticmethod
         def validator():
-            return ValidateParams.Check_lenth(Parser_Results.user)
+            return ValidateParams.Check_length(ParserResults.user, "user")
 
     class Remote_host:
         @staticmethod
         def validator():
-            return ValidateParams.Check_lenth(Parser_Results.host)
+            return ValidateParams.Check_length(ParserResults.host, "host")
 
     @staticmethod
     def do_validator():
--- syncher.py
+++ syncher.py
@@ -130,9 +130,38 @@ class Throw_in(object):
 
 # Validator class
 class ValidateParams(object):
+
     @staticmethod
-    def validator(ParsResult):
-        pass
+    def Check_length(parametr, param_name):
+        magic_number = 777
+        try:
+            magic_number / len(parametr)
+            return parametr
+        except:
+            print("parametr type: {}".format(type(parametr)))
+            print('Unfortunately, you forgot define {}!\nFormat rsync function : rsync [OPTION] ... SRC ... [USER@] HOST:DEST \nPlease, try again'.format(param_name))
+            sys.exit(1)
+
+    class Source_files:
+        @staticmethod
+        def validator():
+            return ValidateParams.Check_length(ParserResults.loc + ParserResults.files, "files")
+
+    class Username:
+        @staticmethod
+        def validator():
+            return ValidateParams.Check_length(ParserResults.user, "user")
+
+    class Remote_host:
+        @staticmethod
+        def validator():
+            return ValidateParams.Check_length(ParserResults.host, "host")
+
+    @staticmethod
+    def do_validator():
+        ValidateParams.Source_files.validator()
+        ValidateParams.Username.validator()
+        ValidateParams.Remote_host.validator()
 
 
 #Interface class
@@ -158,6 +187,7 @@ if __name__ == "__main__":
     print("host: {}".format(ParserResults.host))
     print("dist: {}".format(ParserResults.dist))
 
+    ValidateParams.do_validator()
 
     if ParserResults.port:
         cmd = "rsync {} {} \"ssh -p {}\" {}:{} {}".format(ParserResults.cli, ParserResults.files, ParserResults.port,
