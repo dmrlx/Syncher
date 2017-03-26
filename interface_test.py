1282ba02df5aaffba6e5d3e94456888dc6c0cb33
Alexander Demura <demuraalex@gmail.com>
2017-03-26 19:40:40 +0300

    Update

--- interface_test.py
+++ interface_test.py
@@ -1,3 +1,3 @@
 import syncher
 
-syncher.interface(cli="-avz", files="./tas*", user="root", host="192.168.56.102", dist="/root")
+syncher.interface(cli="-avz", files="./123 ./1* ", user="root", host="192.168.56.102", dist="/root")
--- syncher.py
+++ syncher.py
@@ -19,7 +19,6 @@ class ArgsReceiver(object):
 class ParserResults(object):
     cli = ""        # Params
     password = ""   # Password
-    loc = ""
     files = ""      # Sincable files and folders
     user = ""       # User name
     port = ""       # Port (if needed)
@@ -213,12 +212,6 @@ class Composer(object):
         else:
             cli_param = ""
 
-        if ParserResults.loc:
-            loc_param = ParserResults.loc + " "
-            cli_param += "r"                        # for recursive folders copy
-        else:
-            loc_param = ""
-
         if ParserResults.files:
             files_param = ParserResults.files + " "
         else:
@@ -231,6 +224,15 @@ class Composer(object):
 
         return cmd + cli_param + ssh_param + loc_param + files_param + ParserResults.user + "@" + ParserResults.host + dist_param
 
+class Runner(object):
+    @staticmethod
+    def rsync_runner():
+        ValidateParams.do_validator()
+        cmd = Composer.composer()
+        print(cmd)
+        print(ParserResults.loc)
+        PIPE = subprocess.PIPE
+        p = subprocess.Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=subprocess.STDOUT, close_fds=True)
 
 #Interface function
 def interface(cli=None, password=None, files=None, user=None, port=None, host=None, dist=None):
@@ -241,28 +243,9 @@ def interface(cli=None, password=None, files=None, user=None, port=None, host=No
     ParserResults.port = port
     ParserResults.host = host
     ParserResults.dist = dist
-    ValidateParams.do_validator()
-    cmd = Composer.composer()
-    PIPE = subprocess.PIPE
-    p = subprocess.Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=subprocess.STDOUT, close_fds=True)
-
-
-class Runner(object):
-    def ssh_runner():
-        pass
-    
-    def rsync_runner():
-        pass
-
+    Runner.rsync_runner()
 
 if __name__ == "__main__":
     # Run filling of vars
     Throw_in.parser_results()
-
-    # Run validator
-    ValidateParams.do_validator()
-
-    cmd = Composer.composer()
-    PIPE = subprocess.PIPE
-    p = subprocess.Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=subprocess.STDOUT, close_fds=True)
-    print(p.stderr.read())
+    Runner.rsync_runner()
