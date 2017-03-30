4d27815cf7bd5ba367ea7db9d1cb4b848f2c7f4b
Evgeny <kirgenvall@gmail.com>
2017-03-30 12:55:38 +0300

    Update Parser.py

--- Parser.py
+++ Parser.py
@@ -1,6 +1,4 @@
-"""
 import re
-import sys
 
 
 class ArgsReceiver():
@@ -12,7 +10,6 @@ class ArgsReceiver():
         some_list = ['-t', '*.c', 'foo:/ps', "'-e ssh -P -i'"]
         return first_list
 
-
 class ParserResults():
     cli = ""
     password = ""
@@ -21,17 +18,18 @@ class ParserResults():
     port = ""
     host = ""
     dist = ""
-"""
+
 class Parser(object):
 
-    def execute(self):
-        ParserResults.cli = self.Options.parse(ArgsReceiver.receiver())
-        ParserResults.password = self.Password.parse(ArgsReceiver.receiver())
-        ParserResults.dirs_and_files = self.DirsAndFiles.parse(ArgsReceiver.receiver())
-        ParserResults.user = self.RemoteUser.parse(ArgsReceiver.receiver())
-        ParserResults.port = self.RemotePort.parse(ArgsReceiver.receiver())
-        ParserResults.host = self.RemoteHost.parse(ArgsReceiver.receiver())
-        ParserResults.dist = self.RemoteDirectory.parse(ArgsReceiver.receiver())
+    @staticmethod
+    def execute():
+        ParserResults.cli = Parser.options_parse(ArgsReceiver.receiver())
+        ParserResults.password = Parser.password_parse(ArgsReceiver.receiver())
+        ParserResults.dirs_and_files = Parser.loc_dirs_and_files_parse(ArgsReceiver.receiver())
+        ParserResults.user = Parser.rem_user_parse(ArgsReceiver.receiver())
+        ParserResults.port = Parser.rem_port_parse(ArgsReceiver.receiver())
+        ParserResults.host = Parser.rem_host_parse(ArgsReceiver.receiver())
+        ParserResults.dist = Parser.rem_dirs_and_files_parse(ArgsReceiver.receiver())
 
     @staticmethod
     def check_for_match(pattern, some_list):
@@ -42,28 +40,25 @@ class Parser(object):
                 match_list.append(element)
         return " ".join(match_list)
 
-    class Options():
-        @staticmethod
-        def parse(some_list):
-            options_pattern = r'(^-\w+|^--[\w\-\=]+[\w\d\/\.\_]+|^[\'"].+[\'"]$)'
-            password_pattern = r'-pass=.+'
-            options = Parser.check_for_match(options_pattern, some_list)
-            password = Parser.check_for_match(password_pattern, some_list)
-            return options.replace(password, '')
+    @staticmethod
+    def options_parse(some_list):
+        options_pattern = r'(^-\w+|^--[\w\-\=]+[\w\d\/\.\_]+|^[\'"].+[\'"]$)'
+        password_pattern = r'-pass=.+'
+        options = Parser.check_for_match(options_pattern, some_list)
+        password = Parser.check_for_match(password_pattern, some_list)
+        return options.replace(password, '')
 
-    class Password():
-        @staticmethod
-        def parse(some_list):
-            pattern = r'-pass=.+'
-            return Parser.check_for_match(pattern, some_list)
+    @staticmethod
+    def password_parse(some_list):
+        pattern = r'-pass=.+'
+        return Parser.check_for_match(pattern, some_list)
 
-    class DirsAndFiles():
-        @staticmethod
-        def parse(some_list):
-            pattern = r'^[^-\'"].+'
-            found = Parser.check_for_match(pattern, some_list)
-            pull_remote_info = Parser.pull_remote_info(some_list)
-            return found.replace(pull_remote_info, '')
+    @staticmethod
+    def loc_dirs_and_files_parse(some_list):
+        pattern = r'^[^-\'"].+'
+        found = Parser.check_for_match(pattern, some_list)
+        remote_info = Parser.pull_remote_info(some_list)
+        return found.replace(remote_info, '')
 
     @staticmethod
     def pull_remote_info(some_list):
@@ -78,56 +73,53 @@ class Parser(object):
                 return element
         return ''
 
-    class RemoteUser():
-        @staticmethod
-        def parse(some_list):
-            pattern = r'^\w+[^\:\.\,\@]*'
-            remote_info = Parser.pull_remote_info(some_list)
-            if '@' in remote_info:
-                user = re.match(pattern, remote_info)
-                if user:
-                    return user.group(0)
+    @staticmethod
+    def rem_user_parse(some_list):
+        pattern = r'^\w+[^\:\.\,\@]*'
+        remote_info = Parser.pull_remote_info(some_list)
+        if '@' in remote_info:
+            user = re.match(pattern, remote_info)
+            if user:
+                return user.group(0)
+        return ''
+
+    @staticmethod
+    def rem_port_parse(some_list):
+        user = Parser.rem_user_parse(some_list)
+        remote_info = Parser.pull_remote_info(some_list)
+        if '@' in remote_info:
+            remote_info = remote_info.split('@')
+            port = remote_info[0].lstrip(user)
+            if port:
+                port = port.lstrip(':,.')
+                return port
+        return ''
+
+    @staticmethod
+    def rem_host_parse(some_list):
+        remote_info = Parser.pull_remote_info(some_list)
+        if '@' in remote_info:
+            host_plus_dir = remote_info.split('@')[1].split(':')
+            return host_plus_dir[0]
+        else:
+            host_plus_dir = remote_info.split(':')
+            return host_plus_dir[0]
+
+    @staticmethod
+    def rem_dirs_and_files_parse(some_list):
+        remote_info = Parser.pull_remote_info(some_list)
+        if '@' in remote_info:
+            host_plus_dir = remote_info.split('@')[1].split(':')
+            if len(host_plus_dir) > 1:
+                return host_plus_dir[1]
+        else:
+            host_plus_dir = remote_info.split(':')
+            if len(host_plus_dir) > 1:
+                return host_plus_dir[1]
             return ''
 
-    class RemotePort():
-        @staticmethod
-        def parse(some_list):
-            user = Parser.RemoteUser.parse(some_list)
-            remote_info = Parser.pull_remote_info(some_list)
-            if '@' in remote_info:
-                remote_info = remote_info.split('@')
-                port = remote_info[0].lstrip(user)
-                if port:
-                    port = port.lstrip(':,.')
-                    return port
-            return ''
 
-    class RemoteHost():
-        @staticmethod
-        def parse(some_list):
-            remote_info = Parser.pull_remote_info(some_list)
-            if '@' in remote_info:
-                host_plus_dir = remote_info.split('@')[1].split(':')
-                return host_plus_dir[0]
-            else:
-                host_plus_dir = remote_info.split(':')
-                return host_plus_dir[0]
-
-    class RemoteDirectory():
-        @staticmethod
-        def parse(some_list):
-            remote_info = Parser.pull_remote_info(some_list)
-            if '@' in remote_info:
-                host_plus_dir = remote_info.split('@')[1].split(':')
-                if len(host_plus_dir) > 1:
-                    return host_plus_dir[1]
-            else:
-                host_plus_dir = remote_info.split(':')
-                if len(host_plus_dir) > 1:
-                    return host_plus_dir[1]
-                return ''
-"""
-Parser().execute()
+Parser.execute()
 print('options:{}'.format(ParserResults.cli))
 print('password:{}'.format(ParserResults.password))
 print('dir\'s & files :{}'.format(ParserResults.dirs_and_files))
@@ -135,4 +127,3 @@ print('user:{}'.format(ParserResults.user))
 print('port:{}'.format(ParserResults.port))
 print('host:{}'.format(ParserResults.host))
 print('dist_dir:{}'.format(ParserResults.dist))
-"""
