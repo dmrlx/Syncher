e6c0164c938a54ea46528258381551a42f18c36f
dmrlx <demuraalex@gmail.com>
2017-04-02 15:38:24 +0300

    Changes in subproc_test.py

--- subproc_test.py
+++ subproc_test.py
@@ -1,18 +1,23 @@
+# -*- coding: utf-8 -*-
+
 import subprocess
 
 # Генератор ping'a
 def ping_cmd_generator(host, timer=1):
     ping_cmd = "ping " + str(host) + " -c 1 " + "-W " + str(timer)
-    ping_cmd = ping_cmd.split()
     return ping_cmd
 
 # pusher: Отправляет сформированные в модуле composer команды
 # Возвращает "0" если всё хорошо и "1" если ошибка.
 def pusher(cmd):
+    cmd = cmd.split()
     push = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    output = push.stdout.read().decode()
+    error = push.stderr.read().decode()
     push.wait()
     return push.poll()
 
 if __name__ == "__main__":
-    if pusher(ping_cmd_generator("192.168.56.103")) == 0: # Если пингуется
-        pusher(ssh_connect_cmd_generator(params))
+    pusher(ping_cmd_generator("192.168.56.102"))
+
+
