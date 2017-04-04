dc04844c2a78a5c2b1be7c1c84b3e0f6ccae7135
ItallMakesense <kirgenvall@gmail.com>
2017-04-04 19:37:08 +0300

    SSH Key Transferrer

---
+++ sshKey.py
@@ -0,0 +1,21 @@
+def transfer(username, hostname, password):
+    from paramiko import SSHClient, AutoAddPolicy
+    from socket import gethostbyname, gethostname
+    import installer
+
+    client = SSHClient()
+    client.set_missing_host_key_policy(AutoAddPolicy())
+    client.connect(username=username, hostname=hostname, password=password)
+# Смотрим есть ли файл авторизированных ключей, если нет - создаем
+    auth_keys = 'authorized_keys'
+    stdin, stdout, stderr = client.exec_command('find / -name {}'.format(auth_keys))
+    rem_path_to_keys = stdout.read().decode()
+    if not rem_path_to_keys:
+        client.exec_command('mkdir -p ~/.ssh/ && touch ~/.ssh/{}'.format(auth_keys))
+        stdin, stdout, stderr = client.exec_command('find / -name {}'.format(auth_keys))
+        rem_path_to_keys = stdout.read().decode()
+# Добавляем наш ключ к списку ключей на удаленном хосте
+    client.exec_command("cat {}.pub >> {}".format(installer.pub_keys_path, rem_path_to_keys))
+
+if __name__ == '__main__':
+    transfer('root', 'host', '111')
