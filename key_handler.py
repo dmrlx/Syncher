6cd5bcd3dd92fcd9e9028b6d216e084207be3786
ItallMakesense <kirgenvall@gmail.com>
2017-04-05 14:30:53 +0300

    (\/) UPD (\/)

--- key_handler.py
+++ key_handler.py
@@ -1,15 +1,21 @@
 def interrupt(function):
     import signal
     try:
+# Тут с помощью модуля signal делается перехватчик интеррапт-сигнала (^C),
+# дальнейшие действия определяются методом redirect
         signal.signal(signal.SIGINT, redirect)
         function()
     except RuntimeError:
+# Ловим RuntimeError от ВТОРОГО ПОДРЯД нажатия ctrl+c - команда input()
+# не может добавить ^C в переменную choice (какие-то внутренние шаманства).
+# Место выделено комментарием внизу
         print('But you\'re still trying to stop it... Okay.')
 
 def redirect(signum, frame):
     import sys
     print("\nOperation is still running. It's highly recommended to wait for it to end. ")
     frase = "\bAre you shure you want to stop it? [y/N] ) -> "
+# Поправка input-команды на версию питона
     if sys.version_info.major == 2:
         take_input = raw_input
     elif sys.version_info.major == 3:
@@ -17,8 +23,11 @@ def redirect(signum, frame):
     while True:
 # RuntimeError exception takes place here, due to some stranger things (tried to figure it out for too much time)...
         choice = take_input(frase)
+# Если yes - останавливаем выполнение операции
         if choice.lower() == 'y':
             sys.exit()
+# Если no (по умолчанию) - операция выполняется дальше
+# (с того места, где было словлено прерывание ^C)
         elif choice.lower() == 'n' or choice == '':
             break
         else:
@@ -26,12 +35,12 @@ def redirect(signum, frame):
 
 if __name__ == "__main__":
     from time import sleep
-    import subprocess
+
     def function():
-    #    subprocess.call('ping tut.by', shell=True)
         check = 0
         while True:
             print('{} Goin\'...'.format(check))
             check += 1
             sleep(1)
+
     interrupt(function)
--- ssh_key.py
+++ ssh_key.py
@@ -1,21 +1,22 @@
 def transfer(username, hostname, password):
     from paramiko import SSHClient, AutoAddPolicy
-    from socket import gethostbyname, gethostname
     import installer
-
-    client = SSHClient()
-    client.set_missing_host_key_policy(AutoAddPolicy())
-    client.connect(username=username, hostname=hostname, password=password)
-# Смотрим есть ли файл авторизированных ключей, если нет - создаем
+# Три команды - подключаемся к удаленной машине
+    remote_machine = SSHClient()
+    remote_machine.set_missing_host_key_policy(AutoAddPolicy())
+    remote_machine.connect(username=username, hostname=hostname, password=password)
+# Смотрим на удаленной машине, есть ли файл с паблик ключами подкючавшихся к ней машин
     auth_keys = 'authorized_keys'
-    stdin, stdout, stderr = client.exec_command('find / -name {}'.format(auth_keys))
-    rem_path_to_keys = stdout.read().decode()
-    if not rem_path_to_keys:
-        client.exec_command('mkdir -p ~/.ssh/ && touch ~/.ssh/{}'.format(auth_keys))
-        stdin, stdout, stderr = client.exec_command('find / -name {}'.format(auth_keys))
-        rem_path_to_keys = stdout.read().decode()
-# Добавляем наш ключ к списку ключей на удаленном хосте
-    client.exec_command("cat {}.pub >> {}".format(installer.pub_keys_path, rem_path_to_keys))
+    stdin, stdout, stderr = remote_machine.exec_command('find / -name {}'.format(auth_keys))
+# Если найден, то записываем путь к нему в переменную
+    rem_keys_path = stdout.read().decode()
+# Если такого файла не было найдено, создаем его в папке по умолчанию
+    if not rem_keys_path:
+        remote_machine.exec_command('mkdir -p ~/.ssh/ && touch ~/.ssh/{}'.format(auth_keys))
+        stdin, stdout, stderr = remote_machine.exec_command('find / -name {}'.format(auth_keys))
+        rem_keys_path = stdout.read().decode()
+# Добавляем паблик ключ нашей локальной машины к списку паблик ключей на удаленном хосте
+    remote_machine.exec_command("cat {}.pub >> {}".format(installer.pub_keys_path, rem_keys_path))
 
 if __name__ == '__main__':
     transfer('root', 'host', '111')
