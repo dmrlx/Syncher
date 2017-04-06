9feb708fb370049d051cdced15c28ab399888209
Pollyvitamin <Fedorovichps@gmail.com>
2017-04-06 23:25:16 +0300

    Create installer.py

---
+++ installer.py
@@ -0,0 +1,51 @@
+import paramiko
+from subprocess import call
+from os.path import expanduser
+
+class Installer(object):
+
+    @staticmethod
+    def to_connect(function, host=ParserResults.host, password=ParserResults.password, username=ParserResults.user):
+      def wrapper():
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(host, password=password, username=username)
+        function(ssh=ssh)
+        ssh.close()
+      return wrapper
+
+
+    def install_local_need_soft(need_soft='rsync'):  # Install rsync on local machine
+        try:
+            call('apt-get install -y {} > /dev/null || yum install -y {} > /dev/null'.format(need_soft, need_soft), shell=True)
+            return True
+        except:
+            return False
+
+    @to_connect
+    def install_remote_need_soft(ssh='ssh', need_soft='rsync'):  # Install rsync on remote machine
+        try:
+            stdin, stdout, stderr = ssh.exec_command('apt-get install -y rsync > /dev/null || yum install -y rsync > /dev/null')
+            return True
+        except:
+            return False
+
+    @to_connect
+    def remote_mkdir(ssh='ssh',need_dir=ParserResults.dirs):  # Make remote directory
+        try:
+            stdin, stdout, stderr = ssh.exec_command("mkdir -p {}".format(need_dir))
+            return True
+        except:
+            return False
+
+    @staticmethod
+    def generate_keys():  # Generate SSH keys
+        try:
+            pub_keys_path = "expanduser('~')+'/.ssh/id_rsa'"
+            call('ssh-keygen -t rsa -N "" -f {} > /dev/null'.format(pub_keys_path), shell=True)
+            return True
+        except:
+            return False
+
+if __name__ == "__main__":
+    from variables import ParserResults
