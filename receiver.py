27a93127bf4cb77d5dc752f1dd3011e3dcd80705
vasilmaksimovich <basilioss@gmail.com>
2017-04-06 23:57:11 +0300

    Added comments.

--- receiver.py
+++ receiver.py
@@ -1,6 +1,9 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+r""" Receiver - модуль, принимающий аргументы командной строки.
 
+     Функция receiver передает список аргументов командной строки.
+"""
+    
+    
 import sys
 
 
@@ -9,5 +12,3 @@ class ArgsReceiver(object):  # Получает аргументы от CLI
     @staticmethod
     def receiver():
         return sys.argv[1:]
-
-print(ArgsReceiver.receiver())
